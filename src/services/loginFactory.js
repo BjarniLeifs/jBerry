c203abae68f5d81aa2ad7e78fbb6b6afaf898875
Sindri <Sindri@Sindris-MacBook-Pro.local>
2014-04-29 16:06:23 +0000

    fixing grunt

--- src/services/loginFactory.js
+++ src/services/loginFactory.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-angular.module("BerryApp").factory("loginFactory", [
-	"$location", "$http", "$q",
-	function($location, $http, $q){
-		//User data
-		var data = {
-			Token: "",
-			User: {
-				Email: "",
-				FullName: "",
-				ImageURL: "",
-				Role: "",
-				SSN: "",
-			}
-		};
-		
-		var obj = {
-			//Checkar á user
-			checkUserByUsername: function(user, pass){
-				var loginData = {
-					"user": user,
-					"pass": pass
-				};
-				var deferred = $q.defer();
-				var request = $http({
-					method: "POST", 
-					url: "/api/login",
-					data: loginData
-				});
-				data = data.data;
-				deferred.resolve(request);
-				return deferred.promise;
-			},
-				setData: function(user){
-					data = user;
-				},
-
-				getToken: function(){
-					return data.Token;
-				},
-
-				getUser: function(){
-					return data.User.Username;
-				}
-			};
-		return obj;
-		}]);
-=======
 app.factory("loginFactory", ["$location", "$http", "$q",function($location, $http, $q){
 	var user;
 
@@ -64,4 +16,4 @@ app.factory("loginFactory", ["$location", "$http", "$q",function($location, $htt
 		}
 	};
 }]);
->>>>>>> 401422cff2f83b5ecb3d485e0b71af11ae11b4bc
+
