e41accb10ebc5067ca3367fc1479a4f9cf74f085
steinarv12 <steinarv12@ru.is>
2014-04-29 15:21:36 +0000

    Added Loppy Controller

--- src/services/loginFactory.js
+++ src/services/loginFactory.js
@@ -1,47 +1,18 @@
-angular.module("BerryApp").factory("loginFactory", [
-	"$location", "$http", "$q",
- 	function($location, $http, $q){
- 		//User data
- 		var data = {
- 			Token: "",
- 			User: {
- 				Email: "",
- 				FullName: "",
- 				ImageURL: "",
- 				Role: "",
- 				SSN: "",
- 				Username: ""
- 			}
- 		};
-		
-		var obj = {
-			//Checkar á user
-			checkUserByUsername: function(user, pass){
-				var loginData = {
-					"user": user,
-					"pass": pass
-				};
-				var deferred = $q.defer();
-				var request = $http({
-					method: "POST", 
-					url: "http://dispatch.ru.is/h24/api/v1/login",
-					data: loginData
-				});
-				data = data.data;
-				deferred.resolve(request);
-				return deferred.promise;
-			},
-				setData: function(user){
-					data = user;
-				},
+app.factory("loginFactory", ["$location", "$http", "$q",function($location, $http, $q){
+	var user;
 
-				getToken: function(){
-					return data.Token;
-				},
-
-				getUser: function(){
-					return data.User.Username;
-				}
-			} 
-		return obj;
-	 	}]);
+	return {
+		validUser: function(user, pass){
+			return $http.post({method: "POST", url:"http://dispatch.ru.is/h24/api/v1/login", data: {user:user, password:pass}});
+		},
+		getToken: function(){
+			return user.Token;
+		},
+		setUser: function(newUser) {
+			user = newUser;
+		},
+		getUserName: function(){
+			return user.User.Username;
+		}
+	};
+}]);
