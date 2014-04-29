401422cff2f83b5ecb3d485e0b71af11ae11b4bc
U-Oli-PC\Oli <Oli@Oli-PC.(none)>
2014-04-29 15:21:36 +0000

    Added Loppy Controller

--- src/controllers/logInController.js
+++ src/controllers/logInController.js
@@ -1,23 +1,16 @@
-angular.module("BerryApp").controller("loginController", 
-	["$scope", "$location", "$http", "loginFactory",
-	function($scope, $location, $http, loginFactory){
+app.controller("loginController", ["$scope", "$location", "$http", "loginFactory",function($scope, $location, $http, loginFactory){
 		$scope.user = "";
 		$scope.pass = "";
 
-
-		//TALAR VIÐ FACTORY TIL AÐ ATH HVORT USER ER TIL
 		$scope.connect = function(){
-			console.log("in connect");
-
-			loginFactory.checkUserByUsername($scope.user, $scope.pass).
-				success(function(user, pass){
-				console.log("SUCCESS FUNCTION");
-				$location.path("/#");
-				$scope.$apply();
-
+			userFactory.validUser($scope.user, $scope.pass).success(function(data, status, headers, config){
+				if(status == 200) {
+					userFactory.setUser(data);
+					$location.path("/#");
+					$scope.$apply();
+				}
 			}).error(function(){
-				console.log("error function");
-					// ERROR WINDOW
-				});
-		};	
+			
+			});
+		};
 }]);
