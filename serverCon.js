48495ff7cde5f2ab323ef7aea03f084d81912896
steinarv12 <steinarv12@ru.is>
2014-05-03 15:53:22 +0000

    Server remodeling

--- serverCon.js
+++ serverCon.js
@@ -1,51 +1,46 @@
-var express = require('express'),
-  http = require('http'),
-  path = require('path'),
-  MongoStore = require('connect-mongo')(express),
-  passport = require('passport'),
-  mongodb = require('mongodb'),
-  mongoose = require('mongoose'),
-  LocalStrategy = require('passport-local').Strategy,
-  app = express();
+// server.js
 
+// set up 
+// get all the tools we need
+var express  = require('express'),
+  app       = express(),
+  port      = (process.env.PORT || 3000),
+  mongoose  = require('mongoose'),
+  passport  = require('passport'),
+  flash     = require('connect-flash'),
+  path      = require('path');
 
 var clientDir = path.join(__dirname, '/');
+var configDB = require('./config/database.js');
+
+// configuration
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
 
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.cookieParser('Xwf434FwWDww22a'));
-  app.use(express.session());
+
+  // set up our express application
+  app.use(express.logger('dev')); // log every request to the console
+  app.use(express.cookieParser()); // read cookies (needed for auth)
+  app.use(express.bodyParser()); // get information from html forms
+
+  // required for passport
+  app.use(express.session({ secret: 'fdZwwX3124dcW4324Fwwcl5g5' })); // session secret
   app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
+  app.use(passport.session()); // persistent login sessions
+  app.use(flash()); // use connect-flash for flash messages stored in session
   app.use(express.static(clientDir));
+
 });
 
-app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+// routes 
+require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-app.configure('production', function(){
-    app.use(express.errorHandler());
-});
+// launch 
+app.listen(port);
+console.log('The magic happens on port ' + port);
 
-// passport config
-var Account = require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-
-// mongoose
-//mongoose.connect('mongodb://localhost:27017');
-
-app.get('/', function(req, res) {
-  res.sendfile(path.join(clientDir, 'index.html'));
-});
-
-var server = http.createServer(app);
 
 
 
--- src/controllers/logInController.js
+++ src/controllers/logInController.js
@@ -6,13 +6,11 @@ app.controller("loginController", ["$scope", "$location", "$http", "userFactory"
 	$scope.connect = function(){
 		if(!($scope.email && $scope.pass))
 			return;
-
-
+		console.log($scope.email);
 		userFactory.validUser($scope.email, $scope.pass).success(function(data, status, headers, config){
+			console.log(status);
 			if(status === 200) {
-				userFactory.validUser(data);
-				$location.path("/#/");
-				$scope.$apply();
+				//$location.path("/#/");
 			}
 		}).error(function(){
 			console.log("Error");
