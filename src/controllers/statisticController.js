1af950a4d4e58d9892c97a74829cf20e7a705985
BjarniLeifs <bjarnil10@ru.is>
2014-05-04 00:22:18 +0000

    pull check

--- src/controllers/statisticController.js
+++ src/controllers/statisticController.js
@@ -1,3 +1,3 @@
 app.controller("statisticController", ["$scope", "$location", function($scope, $location) {
-
+$.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
 }]);
