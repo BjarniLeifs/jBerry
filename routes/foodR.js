4f1f485c7532f591e072529ba83b59886e23b1ea
olafur10 <olafur10@ru.is>
2014-05-06 15:32:16 +0000

    Limits the number of query-results to 50

--- routes/foodR.js
+++ routes/foodR.js
@@ -4,8 +4,10 @@ module.exports = function(app) {
 
 	//---able to search the database by querying part of a name, in upper- and lowercase----
 	app.get('/api/food/getByName/:name', function(req, res) {
+		//var ret = new RegExp('^'+req.params.name, 'i');
 		var re = new RegExp(req.params.name, 'i');
-		Matis.find({Nafn:re}, function(err, results) {
+		var q = Matis.find({Nafn:re}, {Nafn:1, _id:0}).sort('Nafn').limit(50);
+		q.exec(function(err, results) {
 			res.send(results);
 		});
 	});
