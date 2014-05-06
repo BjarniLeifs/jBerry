1cbe90e792306cb7ce6440b52e7badb503948ef5
StrawberryLover <olafurkon@gmail.com>
2014-05-06 14:13:00 +0000

    Connect Browse/Search to Main View

--- src/controllers/Main.js
+++ src/controllers/Main.js
@@ -1,5 +1,15 @@
 app.controller("MainController", ["$scope", "$location", function($scope, $location) {
+	$scope.templates = [{url: '/templates/statistic.html'}];
+	$scope.isSideBar = true;
 
+	$scope.setTemplate = function(path, hide) {
+		$scope.templates = [{url: "/templates/" + path}];
 
+		if(hide !== undefined)
+			$scope.isSideBar = !$scope.isSideBar;
+	};
 
+	$scope.getTemplate = function() {
+		return name;
+	};
 }]);
