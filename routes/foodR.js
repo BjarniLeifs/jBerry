f1d4095569cc297dda3cbd23538c15d4e8014701
StrawberryLover <olafurkon@gmail.com>
2014-05-06 19:15:12 +0000

    Fixed Search layout table

--- routes/foodR.js
+++ routes/foodR.js
@@ -2,11 +2,14 @@ var Matis = require('../models/food');
 
 module.exports = function(app) {
 
-	//---able to search the database by querying part of a name, in upper- and lowercase----
+	//Search name 
 	app.get('/api/food/getByName/:name', function(req, res) {
 		//var ret = new RegExp('^'+req.params.name, 'i');
+
 		var re = new RegExp(req.params.name, 'i');
-		var q = Matis.find({Nafn:re}, {Nafn:1, _id:0}).sort('Nafn').limit(50);
+
+		var q = Matis.find({Name: {$regex : ".*"+ req.params.name +".*"}}).limit(50);
+
 		q.exec(function(err, results) {
 			res.send(results);
 		});
