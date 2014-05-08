fe1c78b6f593e20d4bf5a3b63b28f47a069b723f
Sindri55 <sindri12@ru.is>
2014-05-08 12:02:38 +0000

    comments and like to blog

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,12 +2,8 @@ app.controller("blogController", ["$scope", "$location", "$http", "blogFactory",
 	function($scope, $location, $http, blogFactory){
 
 		$scope.blogs="";
-		var id = "";
-		var favs = "";
-		
-		
 
-	console.log("AFLDFNLMDLKFS");
+	console.log("blogController");
 		blogFactory.getBlogs().success(function(data, status, headers, config){
 			if(status === 200) {
 				console.log(status);
@@ -21,16 +17,30 @@ app.controller("blogController", ["$scope", "$location", "$http", "blogFactory",
 		}); 
 
 		$scope.favsCounter = function(_id){
-			
-			console.log("id: " + _id);
-			favs += 1;
-			console.log("favs: " + favs);
+			var id = "";
 			var data = {
 				id   : _id,
-				favs : favs
 			};
 			console.log("data in favsCount: " + data);
-			blogFactory.addFavs(data).success(function(data, status, headers, config){
+			blogFactory.addFavs(data).success(
+				function(data, status, headers, config){
+				console.log(status);
+				}).error(function(){
+					console.log("Error");
+				});
+
+		};
+
+		$scope.addComment = function(_id, comment){
+			var id = "";
+			var comments = [];
+			var data = {
+				id   : _id,
+				comment : comment
+			};
+			console.log("data in comment: " + data.comment);
+			blogFactory.addComments(data).success(
+				function(data, status, headers, config){
 				console.log(status);
 				}).error(function(){
 					console.log("Error");
--- src/services/blogFactory.js
+++ src/services/blogFactory.js
@@ -10,10 +10,15 @@ app.factory("blogFactory", ["$location", "$http", "$q",
 				return $http.get("http://localhost:3000/api/blog");
 			},
 			addFavs: function(data){
-				console.log("factory: " + data.favs + " id: " + data.id);	
-				return $http.post("http://localhost:3000/api/blog",
-				{ data: {_id : data.id, favs : data.favs }});
+				console.log("id: " + data.id);	
+				return $http.put("http://localhost:3000/api/blog/meta/favs",
+				{_id : data.id});
 			},
-	};
+			addComments: function(data){
+				console.log("dataFactory: " + data.comment);	
+				return $http.put("http://localhost:3000/api/blog/comment",
+				{_id : data.id, comment : data.comment});
+			},
+		};
 
 }]);
