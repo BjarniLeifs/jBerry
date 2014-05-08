660987365c3bf61e649b4675ae6fb74a834d3b6c
olafur10 <olafur10@ru.is>
2014-05-08 09:42:03 +0000

    nothing new

--- routes/foodR.js
+++ routes/foodR.js
@@ -4,9 +4,10 @@ module.exports = function(app) {
 
 	//---able to search the database by querying part of a name, in upper- and lowercase----
 	app.get('/api/food/getByName/:name', function(req, res) {
-		//var ret = new RegExp('^'+req.params.name, 'i');
 		var re = new RegExp(req.params.name, 'i');
-		var q = Matis.find({Nafn:re}, {Nafn:1, _id:0}).sort('Nafn').limit(50);
+
+		var q = Matis.find({Name:re}, {Name:1, _id:0, Fita:1}).limit(50);
+		//.sort([[Name.indexOf(re),'1']])
 		q.exec(function(err, results) {
 			res.send(results);
 		});
