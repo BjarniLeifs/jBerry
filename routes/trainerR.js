b07161821697344f9950f2f91c55f3af88e99640
Steinar <steinarv12@ru.is>
2014-05-16 12:42:14 +0000

    Trainer routes added

---
+++ routes/trainerR.js
@@ -0,0 +1,28 @@
+// trainerR.j
+
+var User = require('../models/user');
+
+module.exports = function(app, passport, mongoose) {
+
+  app.get('/api/trainers', function(req, res) {
+    User.find({"isTrainer" : "true"}, function(err, data){
+      res.send(data);
+    });
+  });
+
+  app.post('/api/settrainer', function(req, res) {
+    User.update({"_id" : req.body.ID}, {"isTrainer" : "true"});
+  });
+
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+    return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/notLoggedIn');
+}
